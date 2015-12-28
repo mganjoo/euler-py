05cd05522be51e9108ea2a51a3948023f2a0f1b6
Milind Ganjoo <milind.ganjoo@gmail.com>
2015-12-28 10:35:42 -0800

    Partially vectorized Python solution for P150

--- euler/problem150.py
+++ euler/problem150.py
@@ -1,8 +1,5 @@
+import numpy as np
 import sys
-
-
-def zeros(n):
-    return [[0 for _ in range(n)] for _ in range(n)]
 
 
 def random_number_gen(n):
@@ -14,33 +11,27 @@ def random_number_gen(n):
         yield t - sub
 
 
-def fill_triangle(triangle):
-    n = len(triangle)
+def fill_triangle(triangle_arr):
+    n = len(triangle_arr)
     for i in range(n):
-        triangle[i] += [0] * (n - i - 1)
+        triangle_arr[i] += [0] * (n - i - 1)
+    return np.array(triangle_arr)
 
 
-def find_min_triangle_sum(triangle):
-    fill_triangle(triangle)
+def find_min_triangle_sum(triangle_arr):
+    n = len(triangle_arr)
+    triangle = fill_triangle(triangle_arr)
+    row_sums = np.cumsum(triangle, axis=1)
+    row_sums = np.concatenate((np.zeros((n, 1)), row_sums), axis=1)
 
-    n = len(triangle)
-
-    row_sums = zeros(n)
+    min_sum = np.zeros((n, n))
     for i in range(n):
-        row_sums[i][0] = triangle[i][0]
-        for j in range(1, n):
-            row_sums[i][j] = row_sums[i][j - 1] + triangle[i][j]
-
-    min_sum = sys.maxsize
-    for i in range(n):
+        kv = np.arange(i, n)
         for j in range(i + 1):
-            cur_sum = 0
-            for k in range(i, n):
-                cur_sum += (row_sums[k][k - i + j] -
-                            (row_sums[k][j - 1] if j > 0 else 0))
-                min_sum = min(cur_sum, min_sum)
+            min_sum[i, j] = ((row_sums[kv, kv - i + j + 1] - row_sums[kv, j])
+                             .cumsum().min())
 
-    return min_sum
+    return min_sum.min()
 
 
 # Debug tool
@@ -57,15 +48,19 @@ def print_min_triangle_sum(triangle):
 
 
 if __name__ == "__main__":
-    sample_triangle = [[15],
-                       [-14, -7],
-                       [20, -13, -5],
-                       [-3, 8, 23, -26],
-                       [1, -4, -5, -18, 5],
-                       [-16, 31, 2, 9, 28, 3]]
+    sample_triangle_mini = [[14],
+                            [-15, -7],
+                            [19, -13, -5]]
+    sample_triangle = [[14],
+                       [-15, -7],
+                       [19, -13, -5],
+                       [-4, 8, 23, -26],
+                       [0, -4, -5, -18, 5],
+                       [-17, 31, 2, 9, 28, 3]]
     g = random_number_gen(500500)
-    large_triangle = [[next(g) for j in range(i + 1)] for i in range(1000)]
+    large_triangle = [[next(g) for j in range(i + 1)]
+                      for i in range(1000)]
 
+    print_min_triangle_sum(sample_triangle_mini)
     print_min_triangle_sum(sample_triangle)
-    # Takes forever in Python!
     print_min_triangle_sum(large_triangle)
