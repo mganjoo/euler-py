8e9d1c47c2495d8f3160ad5217c72a98daaba8ef
Milind Ganjoo <milind.ganjoo@gmail.com>
2015-12-30 17:22:04 -0800

    Make Python solution more efficient

--- euler/problem345.py
+++ euler/problem345.py
@@ -3,42 +3,44 @@ from operator import itemgetter
 from collections import Counter
 
 
-def solution(matrix):
-    sorted_matrix = [sorted(enumerate(row), key=itemgetter(1), reverse=True)
-                     for row in matrix]
-    h = len(sorted_matrix)
+def solution(rows):
+    sorted_rows = [sorted(enumerate(row), key=itemgetter(1), reverse=True)
+                   for row in rows]
+    h = len(sorted_rows)
 
     idxs = [0] * h
-    is_unique_pos = False
+    has_dupes = True
 
     diff = {i: 0 for i in range(h)}
-    while not is_unique_pos:
-        is_unique_pos = True
-        pos = [sorted_matrix[i][j][0] for i, j in enumerate(idxs)]
+    while has_dupes:
+        has_dupes = False
+        pos = [sorted_rows[i][j][0] for i, j in enumerate(idxs)]
         counts = Counter(pos)
         next_idx = {}
         for i in range(h):
             if counts[pos[i]] > 1:
                 next_idx[i] = idxs[i] + 1
-                while (sorted_matrix[i][next_idx[i]][0] in pos):
+                while (sorted_rows[i][next_idx[i]][0] in pos):
                     next_idx[i] += 1
-                diff[i] = (sorted_matrix[i][idxs[i]][1] -
-                           sorted_matrix[i][next_idx[i]][1])
-                is_unique_pos = False
+
+                diff[i] = (sorted_rows[i][idxs[i]][1] -
+                           sorted_rows[i][next_idx[i]][1])
+                has_dupes = True
             else:
                 diff.pop(i, None)
-        if not is_unique_pos:
-            min_diff = min(diff.values())
-            i = min(((i, sorted_matrix[i][idxs[i]][1])
-                     for i in range(h) if diff.get(i, -1) == min_diff),
-                    key=itemgetter(1))[0]
-            idxs[i] = next_idx[i]
 
-    return sum(sorted_matrix[i][j][1] for i, j in enumerate(idxs))
+        if has_dupes:
+            # Move index on row with minimum difference from next (and minimum
+            # current value in case of ties)
+            change_i = min(diff,
+                           key=lambda i: (diff[i], sorted_rows[i][idxs[i]][1]))
+            idxs[change_i] = next_idx[change_i]
+
+    return sum(sorted_rows[i][j][1] for i, j in enumerate(idxs))
 
 
 def args():
     fn = pkg_resources.resource_filename(__name__, "data/p345_matrix2.txt")
     with open(fn) as f:
-        matrix = [[int(c) for c in row.split()] for row in f]
-    return [matrix]
+        rows = [[int(c) for c in row.split()] for row in f]
+    return [rows]
